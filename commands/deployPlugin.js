27d119a6d14749a589aa63fef0f5776fe2d3ccdf
Mike Pateras <mipatera@microsoft.com>
2019-07-11 15:37:34 -0400

    Added autoDeploy.

---
+++ commands/deployPlugin.js
@@ -0,0 +1,33 @@
+const exec = require('child_process').exec;
+
+exports.module = class DeployPlugin {
+    apply(compiler) {
+        const localSettings = require("./local.settings");
+        const { publisher, share, token, autoDeploy } = localSettings;
+
+        if (autoDeploy) {
+            compiler.hooks.done.tap("Done", stats => {
+                if (!stats.hasErrors()) {
+                    const manifest = require("../vss-extension.json");
+                    const extensionId = manifest.id;
+                    const extensionVersion = (parseInt(manifest.version) + 1).toString();
+
+                    // Package extension
+                    var createCommand = `tfx extension create --rev-version --manifest-globs vss-extension.json --extension-id ${extensionId}-dev --no-prompt`;
+                    exec(createCommand, (error, stdout, stderr) => {
+                        if (stdout) process.stdout.write('Package created.\r\n');
+                        if (stderr) process.stderr.write(`Package create error: ${error}\r\n`);
+                        if (error) process.stderr.write(`Package create error: ${error}\r\n`);
+                    });
+
+                    const publishCommand = `tfx extension publish --extension-id ${extensionId}-dev --version ${extensionVersion} --publisher ${publisher} --share-with ${share} --token ${token}`;
+                    exec(publishCommand, (error, stdout, stderr) => {
+                        if (stdout) process.stdout.write('Package published.\r\n');
+                        if (stderr) process.stderr.write(`Package publish error: ${error}\r\n`);
+                        if (error) process.stderr.write(`Package publish error: ${error}\r\n`);
+                    });
+                }
+            });
+        }
+    }
+}
