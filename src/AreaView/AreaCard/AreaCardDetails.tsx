a3d8fb4132d787a1fdcb62ac6c78cae835e7b358
Mike Pateras <mipatera@microsoft.com>
2019-07-12 16:32:10 -0400

    Added mutable fields.

--- src/AreaView/AreaCard/AreaCardDetails.tsx
+++ src/AreaView/AreaCard/AreaCardDetails.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import { Objective } from "../../Objective/Objective";
 import { AreaCardProgress } from "./ActiveCardProgress";
+import { TextField } from "azure-devops-ui/TextField";
+import { ObservableValue } from "azure-devops-ui/Core/Observable";
+import { MutableField } from "../../MutableField";
 
 export interface IAreaCardDetailsProps {
 	name: string;
@@ -8,46 +11,23 @@ export interface IAreaCardDetailsProps {
 	objectives: Objective[];
 }
 
-export interface IAreaCardDetailsState {
-    editName: boolean;
-    editDescription: boolean;
-}
-
-export class AreaCardDetails extends React.Component<IAreaCardDetailsProps, IAreaCardDetailsState> {
-	public constructor(props: IAreaCardDetailsProps, state: IAreaCardDetailsState) {
-		super(props, state);
-
-		this.state = {
-			editName: undefined,
-			editDescription: undefined,
-		};
-	}
-
+export class AreaCardDetails extends React.Component<IAreaCardDetailsProps> {
 	public render(): JSX.Element {
-		const { description, objectives } = this.props;
-		
+		const { objectives } = this.props;
+
 		return <>
-			{this.renderName()}
-			<p className="area-description">{description}</p>
+			<h3>
+				<MutableField value={this.props.name} onChange={() => {console.log("saved");}} />
+			</h3>
+			<p>
+				<MutableField value={this.props.description} onChange={() => {}} />
+			</p>
 			<h4>{`${objectives.length} objectives`}</h4>
 			<AreaCardProgress objectives={objectives} />
 		</>;
 	}
 
-	private renderName = (): JSX.Element => {
-        let content = <h3 className="area-name" onClick={this.onNameClick}>{this.props.name}</h3>
+	private save = (): void => {
 
-        if (this.state.editName) {
-            content = <input className="area-name-edit" onChange={this.onNameChange} />
-        }
-
-        return content;
 	};
-
-	private onNameClick = (): void => {
-        this.setState({ editName: true });
-    };
-	
-	private onNameChange = (): void => {
-	}
 }
