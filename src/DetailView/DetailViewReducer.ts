fa0e84302c03d39043efd6a05288fefc710a1467
Becca McHenry <beccam@microsoft.com>
2019-07-16 16:34:50 -0700

    add state based nav

--- src/DetailView/DetailViewReducer.ts
+++ src/DetailView/DetailViewReducer.ts
@@ -1,12 +1,23 @@
+import * as Actions from "./DetailViewActions";
+
 export const detailViewReducer = (state, action) => {
-    switch (action.type) {
-      case 'togglePanel':
-        return {
-          ...state,
-          addPanelExpanded: action.expanded
-        };
-        
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case Actions.navigatePage:
+      return {
+        ...state,
+        pageLocation: action.pageLocation
+      };
+    case 'togglePanel':
+      return {
+        ...state,
+        addPanelExpanded: action.expanded
+      };
+    case Actions.updateArea:
+      return {
+        ...state,
+        area: action.area
+      };
+    default:
+      return state;
+  }
 };
---
+++ src/OKRConstants.ts
@@ -0,0 +1,5 @@
+export class NavigationConstants {
+    public static AreaView = "AreaView";
+    public static DetailView = "DetailView"; 
+    public static Data = "DetailView"; 
+}
