b611517e4c1c55f2c4a17cdb19ee9fcaaf434b52
Mike Pateras <mipatera@microsoft.com>
2019-07-30 10:57:45 -0400

    WIP

--- src/AreaView/AreaCard/Details/AreaCardDetailsStatic.tsx
+++ src/AreaView/AreaCard/Details/AreaCardDetailsStatic.tsx
@@ -1,18 +1,18 @@
 import * as React from "react";
 import { Link } from "azure-devops-ui/Link";
-import { useStateValue } from "../../../StateMangement/StateProvider";
+import { useStateValue, IOKRContext } from "../../../StateMangement/StateProvider";
 import { NavigationConstants } from "../../../OKRConstants";
 import { useAreaCardValue } from "../Provider/AreaCardProvider";
 import { MenuButton, IMenuItem } from "azure-devops-ui/Menu";
 import * as AreaViewActions from "../Provider/AreaCardActions";
 
 export const AreaCardDetailsStatic: React.FunctionComponent = props => {
-	const [{ }, stateDispatcher] = useStateValue();
+	const stateContext: IOKRContext = useStateValue();
 	const [{ area }, areaCardDispatcher] = useAreaCardValue();
 
 	return <>
 		<div className="card-header">
-			<Link onClick={() => { onNameClick(stateDispatcher, area); }}>{area.Name}</Link>
+			<Link onClick={() => { onNameClick(stateContext, area); }}>{area.Name}</Link>
 			<MenuButton hideDropdownIcon={true} contextualMenuProps={{ menuProps: { id: "test", items: getButtons(areaCardDispatcher) } }} iconProps={{ iconName: "MoreVertical" }} />
 		</div>
 	</>;
@@ -35,8 +35,8 @@ function getButtons(areaCardDispatcher): IMenuItem[] {
 	];
 }
 
-const onNameClick = (stateDispatcher, area): void => {
-	stateDispatcher.updateArea(area);
-	stateDispatcher.selectArea(NavigationConstants.DetailView);
+const onNameClick = (stateContext: IOKRContext, area): void => {
+	stateContext.actions.updateSelectedArea({ selectedArea: area});
+	stateContext.actions.navigatePage({ pageLocation: NavigationConstants.DetailView});
 };
 
