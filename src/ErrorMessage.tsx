4ac249c74f0c447baf46db619d6ed22182af071f
Becca McHenry <mchenry.rebecca@gmail.com>
2019-07-31 17:02:59 -0700

    update error message

--- src/ErrorMessage.tsx
+++ src/ErrorMessage.tsx
@@ -3,25 +3,36 @@ import { MessageCard, MessageCardSeverity } from "azure-devops-ui/MessageCard";
 
 export interface ErrorMessageProps {
     error?: Error;
-    onDismiss?: () => void; 
+    onDismiss?: () => void;
 }
 
 export class ErrorMessage extends React.Component<ErrorMessageProps, {}> {
-    public render(): JSX.Element {
 
-        if (this.props.error && this.props.error.message !== "") {
+    private static etagErrorMessage = "Operation failed. Another user has updated this resource before you. Please refresh the page and try again."; 
+
+    public render(): JSX.Element {
+        const { error } = this.props;
+        
+        if (error) {
+
+            // If there is an etag error (a user is trying to update stale data), show a more descriptive message
+            let message = this.checkIfErrorIsETag(error) ? ErrorMessage.etagErrorMessage : error.message;
             return (
                 <MessageCard
                     className="okr-error-message"
                     onDismiss={this.props.onDismiss}
                     severity={MessageCardSeverity.Error}
                 >
-                    {this.props.error.message}
+                    {message}
                 </MessageCard>
             );
         }
         else {
             return null;
         }
+    }
+
+    private checkIfErrorIsETag(error: any): boolean {
+        return error && error.serverError && error.serverError.typeKey === "InvalidDocumentVersionException"; 
     }
 }
